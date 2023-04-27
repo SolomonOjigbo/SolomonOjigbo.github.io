5da5c1317143dd56ac1a881b8df6fe080beabb53
SolomonOjigbo <solomonojigbo@gmail.com>
2023-04-27 23:12:24 +0100

    Changed the base url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
 		>
 			<div className="w-full flex justify-between items-center max-w-7xl mx-auto">
 				<Link
-					to="/my-portfolio"
+					to="/"
 					className="flex items-center gap-2"
 					onClick={() => {
 						setActive("");
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/my-portfolio",
 	plugins: [react()],
+
+	build: {
+		chunkSizeWarningLimit: 1600,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("assets")) {
+						return "assets";
+					}
+				},
+			},
+		},
+	},
 });
